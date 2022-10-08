778a1c5f4cf15166e26716aa397b7a7d29a3684f
Daniel Kuettel <dkuettel@gmail.com>
2022-10-08 19:25:58 +0200

    push/pop is not the right language

--- lua/funky-contexts.lua
+++ lua/funky-contexts.lua
@@ -1,22 +1,21 @@
 local M = {}
 
-local base = { ".", "contexts" }
+-- no trailing slashes
+local base = "./contexts"
+
+-- a context is a path relative to base as a list of names
+-- {"."} is the root context, {".", "some"} is a context with name "some"
 local current_context = {}
 
 local function get_paths(context)
-    local folder = {}
-    vim.list_extend(folder, base)
-    vim.list_extend(folder, context)
-    local file = {}
-    vim.list_extend(file, base)
-    vim.list_extend(file, context)
-    vim.list_extend(file, { "Session.vim" })
-    return table.concat(folder, "/"), table.concat(file, "/")
+    -- return path to folder and path to Session.vim
+    local path = base .. "/" .. table.concat(context, "/")
+    local file = path .. "/Session.vim"
+    return path, file
 end
 
 local function save(context)
     local folder, file = get_paths(context)
-    -- TODO make sure parent folders exist
     -- TODO default mksession and sourcing might do too much (like restore global key mappings)
     vim.fn.system({ "mkdir", "-p", folder })
     vim.cmd("mksession! " .. file)
@@ -24,6 +23,7 @@ end
 
 local function load(context)
     local _, file = get_paths(context)
+    -- TODO default mksession and sourcing might do too much (like restore global key mappings)
     vim.cmd("source " .. file)
 end
 
@@ -31,41 +31,64 @@ local function delete(context)
     local _, file = get_paths(context)
     -- TODO but we can only delete leaves?
     -- or should it drop all below? or disconnect a tree?
-    vim.fn.system({ "rm", file })
+    vim.fn.system({ "rm", "-f", file })
 end
 
-function M.push(name)
-    -- TODO often we want to start clean, or just with one new buffer, the current one, close all other?
+function M.new_copy(name)
+    -- new context, copy of current context, child of current context
     save(current_context)
-    -- TODO name should not exist yet (path)
+    -- TODO check that Session.vim for this context does not exist yet
     table.insert(current_context, name)
     save(current_context)
 end
 
-function M.pop()
+function M.new_empty(name)
+    -- new context, empty vim, child of current context
+    save(current_context)
+    -- TODO check that Session.vim for this context does not exist yet
+    table.insert(current_context, name)
+    vim.cmd("%bd")
+    save(current_context)
+end
+
+function M.new_here(name)
+    -- new context, empty except current buffer, child of current context
+    save(current_context)
+    -- TODO check that Session.vim for this context does not exist yet
+    table.insert(current_context, name)
+    vim.cmd([[
+        tabonly
+        only
+    ]])
+    save(current_context)
+end
+
+function M.drop()
+    -- drop current context, move to parent context
     delete(current_context)
     table.remove(current_context)
     load(current_context)
 end
 
 function M.up()
+    -- switch to parent context, dont drop current context
     save(current_context)
     table.remove(current_context)
     load(current_context)
 end
 
 function M.jump(context)
+    -- jump to any context, dont drop current context
     save(current_context)
     current_context = context
     load(current_context)
 end
 
 function M.list()
-    local found = vim.fn.systemlist("cd " .. table.concat(base, "/") .. "; find . -name Session.vim")
+    local found = vim.fn.systemlist("cd " .. base .. "; find . -name Session.vim")
     local contexts = {}
     for i = 1, #found do
         local context = vim.split(found[i], "/")
-        table.remove(context, 1)
         table.remove(context)
         table.insert(contexts, context)
     end
@@ -73,17 +96,25 @@ function M.list()
 end
 
 function M.setup()
-    vim.api.nvim_create_user_command("Cpush", function(cmd)
-        M.push(cmd["args"][1] or "unnamed")
-    end, { nargs = 1, desc = "push context" })
-    vim.api.nvim_create_user_command("Cpop", M.pop, { nargs = 0, desc = "pop context" })
-    vim.api.nvim_create_user_command("Cup", M.up, { nargs = 0, desc = "one context up" })
-    vim.api.nvim_create_user_command("Cjump", function(cmd)
-        M.jump(cmd["args"])
-    end, { nargs = "+", desc = "jump to any context" })
-    vim.api.nvim_create_user_command("Clist", function()
+    local ucom = vim.api.nvim_create_user_command
+    ucom("CNewCopy", function(cmd)
+        M.new_copy(cmd["args"])
+    end, { nargs = 1 })
+    ucom("CNewEmpty", function(cmd)
+        M.new_empty(cmd["args"])
+    end, { nargs = 1 })
+    ucom("CNewHere", function(cmd)
+        M.new_here(cmd["args"])
+    end, { nargs = 1 })
+    ucom("CDrop", M.drop, {})
+    ucom("CUp", M.up, {})
+    ucom("CJump", function(cmd)
+        -- TODO think about interface, contexts are paths, or lists? or both? what's easier
+        M.jump(cmd["fargs"])
+    end, { nargs = "+" })
+    ucom("CList", function()
         vim.pretty_print(M.list())
-    end, { nargs = 0, desc = "list all contexts" })
+    end, {})
 end
 
 return M
